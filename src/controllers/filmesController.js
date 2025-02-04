63eb5a7a2bde30e6eaa988144e445351b9943d0a
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-02-04 11:47:29 -0300

    feat: ainda realizando.

--- src/controllers/filmesController.js
+++ src/controllers/filmesController.js
@@ -3,4 +3,7 @@ const FilmeList = require("../models/filmesList");
 
 const lista = new FilmeList();
 
-const filme1 = new Filme('Maze Runner: Morrer ou correr','' )
+const filme1 = new Filme('Maze Runner: Morrer ou correr','James Dashner', '1:53', 2 );
+lista.addFilme(filme1);
+
+listaAddFilme( new Filme('Cinderela', 'Charles Perrault', '1:45', 5));
--- src/models/filmes.js
+++ src/models/filmes.js
@@ -1,7 +1,7 @@
 const { v4: uuid4 } = require("uuid");
 
 class Filme {
-    constructor(title, author duration, plays = 0 ) {
+    constructor(title, author, duration, plays = 0 ) {
         this.id = uuid4();
         this.title = title;
         this.author = author;
