9b7b19b16ef9a6e5128589c029d390d0d86f9ea9
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-02-06 15:48:55 -0300

    update filmes.Routes.js

--- src/routes/filmes.Routes.js
+++ src/routes/filmes.Routes.js
@@ -6,7 +6,6 @@ router.get("/filmes", filmeController.getAllFilmes);
 router.post("/filmes", filmeController.addFilme);
 router.put("/filmes/:id", filmeController.updateFilme);
 router.delete("/filmes/:id", filmeController.deleteFilme);
-router.get("/filmes/Top10", filmeController.getTop10Filmes);
 router.get("/filmes/:id", filmeController.getFilmeById);
 
 module.exports = router;
